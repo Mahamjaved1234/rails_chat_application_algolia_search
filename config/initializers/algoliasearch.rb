fc43e0eec0fc1969fef504dab74577b398b7abcd
Maham javed <91803533+Mahamjaved1234@users.noreply.github.com>
2022-10-11 17:14:53 +0500

    Update algoliasearch.rb

--- config/initializers/algoliasearch.rb
+++ config/initializers/algoliasearch.rb
@@ -1,4 +1,4 @@
 AlgoliaSearch.configuration = { 
-    application_id: 'A1G9TBRVMP', 
-    api_key: '35429cfb0c375240d661ebf9b3ac1957' 
+    application_id: 'your app id', 
+    api_key: 'your app key' 
 }
